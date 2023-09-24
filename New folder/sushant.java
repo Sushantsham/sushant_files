329e9bf2570ce23fa9e3a70d0cdaac5bb8e1f85a
Sushantsham <139566889+Sushantsham@users.noreply.github.com>
2023-09-25 01:31:34 +0530

    First_program

---
+++ New folder/sushant.java
@@ -0,0 +1,75 @@
+// first code
+
+/* Java program to push zeroes to back of array */
+import java.io.*;
+  
+class PushZero
+{
+    // Function which pushes all zeros to end of an array.
+    static void pushZerosToEnd(int arr[], int n)
+    {
+        int count = 0;  // Count of non-zero elements
+  
+        // Traverse the array. If element encountered is
+        // non-zero, then replace the element at index 'count'
+        // with this element
+        for (int i = 0; i < n; i++)
+            if (arr[i] != 0)
+                arr[count++] = arr[i]; // here count is
+                                       // incremented
+  
+        // Now all non-zero elements have been shifted to
+        // front and 'count' is set as index of first 0.
+        // Make all elements 0 from count to end.
+        while (count < n)
+            arr[count++] = 0;
+    }
+  
+    /*Driver function to check for above functions*/
+    public static void main (String[] args)
+    {
+        int arr[] = {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9};
+        int n = arr.length;
+        pushZerosToEnd(arr, n);
+        System.out.println("Array after pushing zeros to the back: ");
+        for (int i=0; i<n; i++)
+            System.out.print(arr[i]+" ");
+    }
+}
+/* This code is contributed by Devesh Agrawal */
+
+# Python3 code to move all zeroes
+# at the end of array
+  
+# Function which pushes all
+# zeros to end of an array.
+def pushZerosToEnd(arr, n):
+    count = 0 # Count of non-zero elements
+      
+    # Traverse the array. If element 
+    # encountered is non-zero, then
+    # replace the element at index
+    # 'count' with this element
+    for i in range(n):
+        if arr[i] != 0:
+              
+            # here count is incremented
+            arr[count] = arr[i]
+            count+=1
+      
+    # Now all non-zero elements have been
+    # shifted to front and 'count' is set
+    # as index of first 0. Make all 
+    # elements 0 from count to end.
+    while count < n:
+        arr[count] = 0
+        count += 1
+          
+# Driver code
+arr = [1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9]
+n = len(arr)
+pushZerosToEnd(arr, n)
+print("Array after pushing all zeros to end of array:")
+print(arr)
+  
+# This code is contributed by "Abhishek Sharma 44"
